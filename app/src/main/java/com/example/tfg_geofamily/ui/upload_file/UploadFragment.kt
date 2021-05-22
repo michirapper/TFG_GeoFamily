8de68dce025c3e4e34fe28af519a1065fd9cb435
Miguel Angel Garre <michirapper2001@gmail.com>
2021-05-22 12:28:17 +0200

    subida fichero a firebase

--- app/src/main/java/com/example/tfg_geofamily/ui/upload_file/UploadFragment.kt
+++ app/src/main/java/com/example/tfg_geofamily/ui/upload_file/UploadFragment.kt
@@ -1,17 +1,25 @@
 package com.example.tfg_geofamily.ui.upload_file
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.tfg_geofamily.R
-import com.example.tfg_geofamily.databinding.FragmentMapResultBinding
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.tfg_geofamily.databinding.FragmentUploadBinding
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class UploadFragment : Fragment() {
 
     private lateinit var binding: FragmentUploadBinding
+    lateinit var mUploadBtn: Button
+    lateinit var mStorage: StorageReference
+    private var GALLERY_INTENT = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,30 @@ class UploadFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentUploadBinding.inflate(inflater, container, false)
 
+        mStorage = FirebaseStorage.getInstance().reference
+
+        mUploadBtn = binding.btnSubir
+
+        mUploadBtn.setOnClickListener {
+            var intent: Intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, GALLERY_INTENT)
+        }
+
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY_INTENT && resultCode == Activity.RESULT_OK){
+            var uri: Uri? = data?.data
+            var filePath: StorageReference = mStorage.child("fotos").child(uri?.lastPathSegment.toString())
+            filePath.putFile(uri!!).addOnSuccessListener {
+                Toast.makeText(context, "Foto subida correctamente", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
 
     companion object {
